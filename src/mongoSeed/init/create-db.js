0b6bd3434372ac93e9750e2628632776023317c6
acatchpole <adamantiumite@gmail.com>
2024-09-19 13:17:30 -0700

    one more seed data update

--- src/mongoSeed/init/create-db.js
+++ src/mongoSeed/init/create-db.js
@@ -102,13 +102,13 @@ db.advisories.insertMany([
     details:
       "pH balance of lake recorded at 0 due to sulfur from volcanic activity",
   },
-    {
+  {
     latitude: 49.36088498,
     longitude: -123.1092398,
     submissionTime: new Date(2024, 8, 11, 14, 7, 29),
     expirationTime: new Date(2024, 8, 25, 9, 12, 54),
     eventType: "Drinking Water",
-    details: "High traces of norovirus detected in water supply"
+    details: "High traces of norovirus detected in water supply",
   },
   {
     latitude: 49.58404622,
@@ -116,7 +116,8 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 8, 17, 8, 33, 19),
     expirationTime: new Date(2024, 9, 1, 17, 49, 37),
     eventType: "Animal Sighting",
-    details: "Sasquatch possibly sighted at the edge of forest. Proceed with caution"
+    details:
+      "Sasquatch possibly sighted at the edge of forest. Proceed with caution",
   },
   {
     latitude: 48.88879286,
@@ -124,7 +125,8 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 8, 12, 16, 20, 7),
     expirationTime: new Date(2024, 8, 26, 13, 14, 23),
     eventType: "Swimming",
-    details: "Recent volcanic activity created highly acidic carbonic waters that will melt skin off the bone"
+    details:
+      "Recent volcanic activity created highly acidic carbonic waters that will melt skin off the bone",
   },
   {
     latitude: 49.2039286,
@@ -132,7 +134,7 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 8, 9, 10, 42, 18),
     expirationTime: new Date(2024, 8, 23, 19, 34, 51),
     eventType: "Drinking Water",
-    details: "Water is purple, we don't know why, boil it I guess"
+    details: "Water is purple, we don't know why, boil it I guess",
   },
   {
     latitude: 49.02160488,
@@ -140,7 +142,8 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 7, 31, 6, 19, 3),
     expirationTime: new Date(2024, 8, 14, 17, 58, 29),
     eventType: "Animal Sighting",
-    details: "Large gathering of poisonous red jellyfish seen off coast of beach."
+    details:
+      "Large gathering of poisonous red jellyfish seen off coast of beach.",
   },
   {
     latitude: 48.80042143,
@@ -148,7 +151,7 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 8, 5, 22, 51, 36),
     expirationTime: new Date(2024, 8, 19, 7, 44, 12),
     eventType: "Swimming",
-    details: "Lake dried up. No swimming"
+    details: "Lake dried up. No swimming",
   },
   {
     latitude: 49.06086151,
@@ -156,7 +159,8 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 8, 9, 5, 12, 27),
     expirationTime: new Date(2024, 8, 23, 12, 32, 45),
     eventType: "Drinking Water",
-    details: "700 barrels of wine has crashed into water supply, contaminating local drinking water"
+    details:
+      "700 barrels of wine has crashed into water supply, contaminating local drinking water",
   },
   {
     latitude: 48.63588153,
@@ -164,7 +168,8 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 7, 30, 20, 30, 59),
     expirationTime: new Date(2024, 8, 13, 9, 45, 13),
     eventType: "Animal Sighting",
-    details: "Red Ants and Black Ants have ended their thousand-year war and are combining forces, the hills are really big"
+    details:
+      "Red Ants and Black Ants have ended their thousand-year war and are combining forces, the hills are really big",
   },
   {
     latitude: 49.35601779,
@@ -172,7 +177,8 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 8, 5, 16, 26, 14),
     expirationTime: new Date(2024, 8, 19, 8, 32, 7),
     eventType: "Swimming",
-    details: "Active landslide has caused river to flow into lake, creating currents and unclear water"
+    details:
+      "Active landslide has caused river to flow into lake, creating currents and unclear water",
   },
   {
     latitude: 49.01828909,
@@ -180,7 +186,8 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 8, 15, 13, 4, 19),
     expirationTime: new Date(2024, 9, 1, 10, 56, 43),
     eventType: "Drinking Water",
-    details: "Lead from concrete pipes seeping into water, boiling won't help, use bottled water for consumption."
+    details:
+      "Lead from concrete pipes seeping into water, boiling won't help, use bottled water for consumption.",
   },
   {
     latitude: 48.59665847,
@@ -188,7 +195,7 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 8, 1, 18, 12, 17),
     expirationTime: new Date(2024, 9, 1, 15, 21, 51),
     eventType: "Animal Sighting",
-    details: "Jersey Devil Spotted stealing apples from orchard"
+    details: "Jersey Devil Spotted stealing apples from orchard",
   },
   {
     latitude: 48.44858106,
@@ -196,7 +203,8 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 7, 22, 21, 55, 29),
     expirationTime: new Date(2024, 8, 5, 16, 33, 41),
     eventType: "Swimming",
-    details: "A recent fissure in lake ground has caused a massive whirlpool that is pulling debris and living things into the unknown. Bring a floaty."
+    details:
+      "A recent fissure in lake ground has caused a massive whirlpool that is pulling debris and living things into the unknown. Bring a floaty.",
   },
   {
     latitude: 48.55408406,
@@ -204,7 +212,8 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 8, 6, 14, 18, 11),
     expirationTime: new Date(2024, 8, 20, 9, 34, 52),
     eventType: "Drinking Water",
-    details: "Kool-Aid Man has taken over nearby reservoir, the water is red but it's fine to drink. Oh Yeah!"
+    details:
+      "Kool-Aid Man has taken over nearby reservoir, the water is red but it's fine to drink. Oh Yeah!",
   },
   {
     latitude: 48.91259788,
@@ -212,7 +221,7 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 8, 11, 19, 48, 25),
     expirationTime: new Date(2024, 8, 25, 17, 14, 57),
     eventType: "Animal Sighting",
-    details: "A wild Lugia has appeared!"
+    details: "A wild Lugia has appeared!",
   },
   {
     latitude: 48.51344607,
@@ -220,7 +229,7 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 8, 17, 8, 24, 39),
     expirationTime: new Date(2024, 9, 1, 7, 16, 45),
     eventType: "Swimming",
-    details: "Lake is Frozen, No Diving!"
+    details: "Lake is Frozen, No Diving!",
   },
   {
     latitude: 49.1349886,
@@ -228,7 +237,8 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 7, 31, 11, 12, 7),
     expirationTime: new Date(2024, 7, 14, 13, 24, 18),
     eventType: "Drinking Water",
-    details: "Tea bags have been prepped in the mugs. Don't forget to add honey!"
+    details:
+      "Tea bags have been prepped in the mugs. Don't forget to add honey!",
   },
   {
     latitude: 49.18443958,
@@ -236,7 +246,7 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 8, 9, 9, 46, 35),
     expirationTime: new Date(2024, 8, 23, 8, 28, 51),
     eventType: "Animal Sighting",
-    details: "Ancient Red Dragon brooding near bank"
+    details: "Ancient Red Dragon brooding near bank",
   },
   {
     latitude: 48.98058525,
@@ -244,7 +254,7 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 7, 30, 21, 12, 15),
     expirationTime: new Date(2024, 7, 14, 10, 54, 39),
     eventType: "Swimming",
-    details: "Tsunami warning off beach"
+    details: "Tsunami warning off beach",
   },
   {
     latitude: 49.3585375,
@@ -252,6 +262,15 @@ db.advisories.insertMany([
     submissionTime: new Date(2024, 8, 5, 13, 34, 41),
     expirationTime: new Date(2024, 8, 19, 14, 7, 59),
     eventType: "Drinking Water",
-    details: "Water smells bad, but after boiling, it's okay and not smelly."
-  }
+    details: "Water smells bad, but after boiling, it's okay and not smelly.",
+  },
+  {
+    latitude: 48.52973926317433,
+    longitude: -123.39506426460531,
+    submissionTime: new Date(2024, 9, 19, 13, 13, 41),
+    expirationTime: new Date(2024, 9, 21, 0, 0, 0),
+    eventType: "Swimming",
+    details:
+      "Lost merfolk are battling a local lake serpent - swimming is NOT advised. Making wagers on who will win is encouraged",
+  },
 ]);
